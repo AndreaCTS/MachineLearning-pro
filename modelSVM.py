a0793dd9a2085b4577537c58b08472545dd6bde8
AndreaCTS <90526089+AndreaCTS@users.noreply.github.com>
2024-06-08 12:53:51 -0500

    first try

---
+++ modelSVM.py
@@ -0,0 +1,50 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+import joblib
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+
+
+# Función para cargar y preprocesar el dataset MNIST
+def load_mnist_data():
+    mnist = tf.keras.datasets.mnist
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    # Aplanar las imágenes
+    # Redimensionar las imágenes a 28x28 y aplanar
+    X_train = X_train.reshape(-1, 28*28).astype('float32') 
+    X_test = X_test.reshape(-1, 28*28).astype('float32') 
+    return X_train, X_test, y_train, y_test
+
+# Cargar el dataset MNIST
+X_train, X_test, y_train, y_test = load_mnist_data()
+
+
+# Estandarizar las características
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+param_grid_poly = {
+    'C': [10],
+    'gamma': ['scale']
+}
+
+
+# Ajustar el modelo SVM con kernel polinomial
+grid_poly = GridSearchCV(SVC(kernel='poly'), param_grid_poly, refit=True, cv=5, n_jobs=-1)
+grid_poly.fit(X_train, y_train)
+print(f"Best parameters for polynomial kernel: {grid_poly.best_params_}")
+y_predPoly = grid_poly.predict(X_test)
+print(f'Accuracy with  Standard in POLY: {accuracy_score(y_test, y_predPoly)}')
+
+
+
+# Guardar el modelo entrenado y el scaler
+joblib.dump(grid_poly, "svm_digit_classifier.pkl")
+joblib.dump(scaler, "scaler_28x28.pkl")
